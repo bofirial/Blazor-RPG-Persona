58fc35300b914f79dfc2ba46bda586f7018b1088
James Schafer <james@jschafer.net>
2018-11-28 07:24:22 -0500

    Refactoring the User Form to it's own component

---
+++ Blazor-RPG-Persona-Common/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor_RPG_Persona_Common.Models
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+
+        public int BirthMonth { get; set; }
+
+        public int BirthDay { get; set; }
+
+        public string FavoriteFood { get; set; }
+    }
+}
