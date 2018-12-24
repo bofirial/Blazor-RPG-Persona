4c9da6ad054a250f8856f1390299a25174cd8ce8
James Schafer <james@jschafer.net>
2018-12-24 00:16:09 -0500

    Added CollapsiblePanel Component and Implementation

---
+++ TechnologyCharacterGenerator.Foundation/content/foundation.js
@@ -0,0 +1,22 @@
+﻿
+window.foundation = {
+    collapsiblePanel: {
+        init: function (dotNetComponent, componentId, isCollapsed) {
+            $(function() {
+                var $collapsiblePanel = $('#' + componentId);
+
+                $collapsiblePanel.collapse(isCollapsed ? 'hide' : 'show');
+
+                $collapsiblePanel.on('hide.bs.collapse',
+                    function() {
+                        dotNetComponent.invokeMethodAsync('UpdateIsCollapsed', true);
+                    });
+
+                $collapsiblePanel.on('show.bs.collapse',
+                    function() {
+                        dotNetComponent.invokeMethodAsync('UpdateIsCollapsed', false);
+                    });
+            });
+        }
+    }
+};
